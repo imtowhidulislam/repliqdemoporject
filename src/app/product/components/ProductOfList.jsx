79b0450f732907ab0df6343e53ebe801abc1d02c
towhid <niamaaktar@gmail.com>
2023-07-25 14:37:00 +0600

    --amend

--- src/app/product/components/ProductOfList.jsx
+++ src/app/product/components/ProductOfList.jsx
@@ -1,21 +1,9 @@
 "use client";
 import React, { useEffect, useState } from "react";
-// import Button from '../../Util/Button';
-import { BiArrowToLeft, BiArrowToRight } from "react-icons/bi";
-import Link from "next/link";
-import Image from "next/image";
-import dynamic from "next/dynamic";
 import { toast } from "react-hot-toast";
-import Loading from "../loading";
-import { HiShoppingCart, HiOutlineHeart, HiHeart } from "react-icons/hi";
-import FavoriteBtn from "./FavoriteBtn";
-import ButtonFilled from "../../../common/ButtonFilled";
-
-const LazyComponent = dynamic(() => import("../../../common/LazyComponent"));
+import ProductCard from "@/common/ProductCard";
 
 const ProductOfList = ({ product, isLoading, cart, setCart }) => {
-  // const [productData, setProductData] = useState([]);
-
   const getProduct = (id) => {
     try {
       let quantity = 0;
@@ -54,59 +42,15 @@ const ProductOfList = ({ product, isLoading, cart, setCart }) => {
           } = singleProduct;
           const titleLength = title.split(" ").slice(0, 5).join(" ");
           return (
-            <div
-              key={id}
-              className="card relative z-10 flex h-max animate-moveUp flex-col items-center justify-between gap-2 bg-nutral3 transition-colors duration-100 ease-in-out hover:bg-[#f5f5f5]"
-            >
-              <div className="absolute right-3 top-3">
-                <FavoriteBtn />
-              </div>
-              <Link href={`/product/${id}`} className="">
-                <div>
-                  <LazyComponent img={img} />
-
-                  <div className="z-20 px-4 pb-4 pt-2 text-nutral2">
-                    <h2 className="mt-2 text-base font-semibold uppercase">
-                      {cat}
-                    </h2>
-                    <div>
-                      <h2 className="line-clamp-1 sm:text-lg">
-                        {" "}
-                        {title.split(" ").length <= 5
-                          ? `${titleLength}`
-                          : `${titleLength}...`}
-                      </h2>
-                    </div>
-                    <div className="flex items-center justify-between gap-4 py-2">
-                      <h2>
-                        {" "}
-                        <span className="text-lg font-bold text-primary">
-                          $ {price}
-                        </span>
-                      </h2>
-                      <p className="text-base">
-                        Rating :{" "}
-                        <span className="text-base font-bold text-nutral2">
-                          {rating.rate}
-                        </span>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </Link>
-              <div className="flex w-full items-center justify-between px-4 pb-4">
-                <ButtonFilled
-                  btnLebel="add to cart"
-                  btnType="button"
-                  onClick={() => getProduct(id)}
-                  classNames="group flex w-full cursor-pointer items-center justify-center gap-2 rounded-md border border-primary bg-transparent  px-4 py-2 text-sm font-bold capitalize text-primary drop-shadow-lg transition-all duration-200 ease-in-out hover:border-transparent hover:bg-nutral3 hover:text-primary hover:drop-shadow-md"
-                >
-                  <span>
-                    <HiShoppingCart className="text-2xl text-primary " />
-                  </span>
-                </ButtonFilled>
-              </div>
-            </div>
+            <ProductCard
+              addProduct={() => getProduct(id)}
+              id={id}
+              title={title}
+              img={img}
+              price={price}
+              rating={rating}
+              category={cat}
+            />
           );
         })
       )}
--- src/components/HomeProduct/page.jsx
+++ src/components/HomeProduct/page.jsx
@@ -11,6 +11,7 @@ import Loading from "@/app/product/loading";
 import FavoriteBtn from "@/app/Product/components/FavoriteBtn";
 import { HiShoppingCart } from "react-icons/hi";
 import ButtonFilled from "../../common/ButtonFilled";
+import ProductCard from "@/common/ProductCard";
 const LazyComponent = dynamic(() => import("../../common/LazyComponent"));
 
 const HomeTopratedProduct = () => {
@@ -63,7 +64,7 @@ const HomeTopratedProduct = () => {
     <div>
       <div>
         <div className="container py-20">
-          <div className="mb-8 px-4 sm:px-0 md:mb-12">
+          <div className="px-4 mb-8 sm:px-0 md:mb-12">
             <h2
               className={
                 isLoading
@@ -75,9 +76,9 @@ const HomeTopratedProduct = () => {
             </h2>
           </div>
           {isLoading ? (
-            <h2 className="text-center text-2xl font-bold">Loading...</h2>
+            <h2 className="text-2xl font-bold text-center">Loading...</h2>
           ) : (
-            <div className="ms:px-0 grid w-full grid-cols-productLayout place-items-center gap-4 overflow-hidden px-4 sm:grid-cols-productLayout">
+            <div className="grid w-full gap-4 px-4 overflow-hidden ms:px-0 grid-cols-productLayout place-items-center sm:grid-cols-productLayout">
               {topRate?.map((topProduct, inx) => {
                 const {
                   id,
@@ -89,58 +90,17 @@ const HomeTopratedProduct = () => {
                 } = topProduct;
                 const titleLength = title.split(" ").slice(0, 5).join(" ");
                 return (
-                  <div
-                    key={id}
-                    className={`card relative z-10 flex h-max animate-moveUp flex-col items-center justify-between gap-2 bg-nutral3 transition-colors duration-100 ease-in-out hover:bg-[#f5f5f5]`}
-                  >
-                    <div className="absolute right-3 top-3">
-                      <FavoriteBtn />
-                    </div>
-                    <Link href={`/product/${id}`} className="">
-                      <div>
-                        <LazyComponent img={img} />
-                        <div className="z-20 px-4 pb-4 pt-2 text-nutral2">
-                          <h2 className="mt-2 text-base font-semibold uppercase">
-                            {cat}
-                          </h2>
-                          <div>
-                            <h2 className="line-clamp-1 sm:text-lg">
-                              {" "}
-                              {title.split(" ").length <= 5
-                                ? `${titleLength}`
-                                : `${titleLength}...`}
-                            </h2>
-                          </div>
-                          <div className="flex items-center justify-between gap-4 py-2">
-                            <h2>
-                              {" "}
-                              <span className="text-lg font-bold text-primary">
-                                $ {price}
-                              </span>
-                            </h2>
-                            <p className="text-base">
-                              Rating :{" "}
-                              <span className="text-base font-bold text-nutral2">
-                                {rating.rate}
-                              </span>
-                            </p>
-                          </div>
-                        </div>
-                      </div>
-                    </Link>
-                    <div className="flex w-full items-center justify-between px-4 pb-4">
-                      <ButtonFilled
-                        btnType="button"
-                        btnLebel="add to cart"
-                        onClick={() => addToCart(id)}
-                        classNames="group flex w-full cursor-pointer items-center justify-center gap-2 rounded-md border border-primary bg-transparent  px-4 py-2 text-sm font-bold capitalize text-primary drop-shadow-lg transition-all duration-200 ease-in-out hover:border-transparent hover:bg-nutral3 hover:text-primary hover:drop-shadow-md"
-                      >
-                        <span>
-                          <HiShoppingCart className="text-2xl text-primary " />
-                        </span>
-                      </ButtonFilled>
-                    </div>
-                  </div>
+                  <>
+                    <ProductCard
+                      addProduct={() => addToCart(id)}
+                      id={id}
+                      title={title}
+                      price={price}
+                      rating={rating}
+                      img={img}
+                      category={cat}
+                    />
+                  </>
                 );
               })}
             </div>
