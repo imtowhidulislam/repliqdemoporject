a0057d072d7e080581b78e2306a0b8bf1809b053
towhid <niamaaktar@gmail.com>
2023-07-28 18:45:01 +0600

    routing issuse has resolved from the dashboard

--- src/app/dashboard/components/common/Navbar/page.jsx
+++ src/app/dashboard/components/common/Navbar/page.jsx
@@ -12,39 +12,41 @@ const dashBoard = [
   },
   {
     id: 2,
-    route: "/dashboard/addproduct",
+    route: "/dashboard/create-product",
     routeName: "Create Product",
   },
   {
     id: 3,
-    route: "/dashboard/user",
+    route: "/dashboard/register-user",
     routeName: "Register User",
   },
   {
     id: 4,
-    route: "/dashboard/newproduct",
+    route: "/dashboard/created-product",
     routeName: "Created Product",
   },
-  {
+/*   {
     id: 5,
     route: "/dashboard/image-upload",
-    routeName: "Upload Image",
-  },
+    routeName: "Image Upload",
+  }, */
 ];
 const DashboardNavbar = () => {
   const currRoute = usePathname().split("/").slice(-1)[0].toLowerCase();
+  console.log(currRoute);
   return (
     <div>
       <div className="grid w-full gap-3 mt-16 place-items-start md:mt-0">
         {dashBoard.map((routes) => {
           const { id, route, routeName } = routes;
-          // console.log(routeName.toLowerCase().split(" ").join(""));
+          // console.log(routeName);
+          // console.log(routeName.toLowerCase().split(" ").join("-"));
           return (
             <Link
               key={id}
               href={route}
               className={
-                currRoute === routeName.toLowerCase().split(" ").join("")
+                currRoute === routeName?.toLowerCase().split(" ").join("-")
                   ? "w-full cursor-pointer rounded-md bg-primary  px-3 lg:px-4 xl:px-6 py-2 text-left font-bold capitalize text-nutral3 shadow-nutral2 drop-shadow-md hover:border-transparent hover:bg-primary/20 hover:text-primary"
                   : "w-full cursor-pointer rounded-md bg-transparent px-3 lg:px-4 xl:px-6 py-2 text-left font-bold capitalize text-nutral2 shadow-nutral2 drop-shadow-md hover:border-transparent hover:bg-primary/20 hover:text-primary"
               }
