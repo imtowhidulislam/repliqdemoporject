b08ae31bc7aa86c54f2056550296c3e8d159cea6
towhid <niamaaktar@gmail.com>
2023-08-01 15:12:58 +0600

    image size of product card has changed

--- src/common/LazyComponent.js
+++ src/common/LazyComponent.js
@@ -3,19 +3,19 @@ import Image from "next/image";
 import FavoriteBtn from "@/app/product/components/FavoriteBtn";
 
 const LazyComponent = ({ img }) => {
-  return (
-    <>
-      <div className="grid max-w-xs  place-items-center h-60">
-        <Image
-          className="block object-cover objece-center aspect-square h-60"
-          src={img}
-          alt=""
-          width={300}
-          height={400}
-        />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="grid max-w-xs  place-items-center  overflow-hidden">
+                <Image
+                    className="object-cover object-center aspect-square"
+                    src={img}
+                    alt=""
+                    width={250}
+                    height={200}
+                />
+            </div>
+        </>
+    );
 };
 
 export default LazyComponent;
